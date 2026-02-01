3ffd694ef52dfe8161a2819559eb1168206d2007
Saurabh Lakshkar <saurabhlakshkar321@gmail.com>
2026-02-01 23:53:37 +0530

    k

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 function App() {
+  function btnclick(){
+    console.log('button clicked')
+  }
   return (
-    <div className='bg-red-400'>App</div>
+    <div className='bg-red-400'>
+      <button onClick={btnclick}>click here</button>
+    </div>
   )
 }
 
