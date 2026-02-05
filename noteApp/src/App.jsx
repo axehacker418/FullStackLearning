12f463cbbcb11b81eaea09ded584567c945f0313
Saurabh Lakshkar <saurabhlakshkar321@gmail.com>
2026-02-05 15:02:18 +0530

    feb5

--- noteApp/src/App.jsx
+++ noteApp/src/App.jsx
@@ -1,36 +1,58 @@
 import React from 'react'
 
 function App() {
-  const submitHandler= (e) =>{
+  const submitHandler = (e) => {
     e.preventDefault()
-    console.log('form submitted...');
-
+    console.log('form submitted...')
   }
+
   return (
-    <div className='h-screen  bg-amber-100 text-white '>
-      <form onSubmit={(e)=>{
-        submitHandler(e);
+    <div className="min-h-screen flex flex-col lg:flex-row bg-amber-100 text-white gap-6 p-6">
 
-      }} className='flex justify-between items-start  rounded-2xl p-10  bg-amber-100'>
-         <div className=' flex pl-3 w-1/2 items-center flex-col'>
-        <img className='h-60 ' src="https://static.vecteezy.com/system/resources/thumbnails/049/578/155/small/a-black-and-white-drawing-of-a-man-writing-png.png" alt="Note" />
+      {/* FORM SECTION */}
+      <form
+        onSubmit={submitHandler}
+        className="flex flex-col lg:flex-row w-full lg:w-2/3 gap-6 rounded-2xl p-6 bg-amber-100"
+      >
+        {/* IMAGE */}
+        <div className="flex w-full lg:w-1/2 items-center justify-center">
+          <img
+            className="h-40 lg:h-60"
+            src="https://static.vecteezy.com/system/resources/thumbnails/049/578/155/small/a-black-and-white-drawing-of-a-man-writing-png.png"
+            alt="Note"
+          />
         </div>
-        
 
-        <div className='flex gap-4 rounded-2xl px-3 bg-green-100 w-1/2 items-center flex-col'>
-        <input className='px-5 mt-3 w-full py-2 border-2 rounded text-blue-800 ' type='text' placeholder='write notes heading..' />
-        
-        <textarea className='px-5 h-20 w-full py-0 text-blue-800 border-2 rounded' placeholder='enter details' name='' id=''></textarea>
-        <button className=' bg-cyan-700 mb-4 w-full py-2 pb-3 text-white rounded-2xl'>Add Note</button>
+        {/* INPUTS */}
+        <div className="flex gap-4 rounded-2xl px-4 bg-green-100 w-full lg:w-1/2 items-center flex-col">
+          <input
+            className="px-5 mt-3 w-full py-2 border-2 rounded text-blue-800"
+            type="text"
+            placeholder="write notes heading.."
+          />
 
+          <textarea
+            className="px-5 h-24 w-full py-2 text-blue-800 border-2 rounded"
+            placeholder="enter details"
+          ></textarea>
+
+          <button className="bg-cyan-700 mb-4 w-full py-2 text-white rounded-2xl">
+            Add Note
+          </button>
         </div>
-        
-      
-       
       </form>
-      
-    
-      
+
+      {/* NOTES SECTION */}
+      <div className="w-full lg:w-1/3 p-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
+          <div className="bg-white p-6 rounded-2xl h-32"></div>
+          <div className="bg-white p-6 rounded-2xl h-32"></div>
+          <div className="bg-white p-6 rounded-2xl h-32"></div>
+          <div className="bg-white p-6 rounded-2xl h-32"></div>
+        </div>
+      </div>
+
+
     </div>
   )
 }
